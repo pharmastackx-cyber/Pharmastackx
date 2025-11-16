736339a7df178568960f27202751c10fde563d14
pogiemudia <pogiemudia@gmail.com>
2025-11-16 19:22:08 +0000

    feat: integrate Google Analytics and update favicon

---
+++ src/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+
+"use client"
+
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
+
+import { pageview } from "@/lib/gtag"
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString()
+    pageview(url)
+  }, [pathname, searchParams])
+
+  return (
+    <>
+      {/* Add your Google Analytics tracking code here */}
+    </>
+  )
+}
